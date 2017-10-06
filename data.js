1b9645fd01acc407c1d4a482a58b7b683670f39f
hairmot <peter.colley@hotmail.com>
2017-10-06 15:53:55 +0100

    more

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ var childrenArray = [{name: '1'},{name: '2'},{name: '3'}];
 var bigArray = [{name: '1', children:childrenArray},{name: '2',children:childrenArray},{name: '3',children:childrenArray}];
 for (var i = 0; i < 4; i++) {
     var dynObj = {
-        name: String.fromCharCode(97 + i),
+        name: 'Domain ' + String.fromCharCode(97 + i).toUpperCase(),
         children : bigArray
     }
     output[i] = dynObj;
